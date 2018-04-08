52018734d98f55801511e09c728c32af1037aaad
zhouyong10 <1059981087@qq.com>
2018-04-08 17:15:05 +0800

    编写注册页面

--- app/src/main/java/com/chuangyouclub/chuangyouhui/LoginActivity.java
+++ app/src/main/java/com/chuangyouclub/chuangyouhui/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.chuangyouclub.chuangyouhui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_log_in = (Button) findViewById(R.id.log_in);
+        btn_log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, LogupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
